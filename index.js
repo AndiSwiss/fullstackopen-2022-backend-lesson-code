4577397100d98324c1657196335b20220a4c8522
Andreas Ambühl <36681000+andiswiss@users.noreply.github.com>
2022-07-17 14:00:27 +0200

    introduce mongoose validation

--- index.js
+++ index.js
@@ -68,14 +68,13 @@ app.post('/api/notes', (request, response, next) => {
  * PUT: Change a note
  */
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
+  const {content, important} = request.body
 
-  const note = {
-    content: body.content,
-    important: body.important || false,
-  }
-
-  Note.findByIdAndUpdate(request.params.id, note, {new: true})
+  Note.findByIdAndUpdate(
+    request.params.id,
+    {content, important},
+    {new: true, runValidators: true, context: 'query'}
+  )
     .then(updatedNote => {
       response.json(updatedNote)
     })
@@ -104,10 +103,11 @@ app.use(unknownEndpoint)
  */
 const errorHandler = (error, request, response, next) => {
   console.log(error)
-  response.status(400).send({
-    error: error.name,
-    errorMessage: error.message
-  })
+
+  if (error.name === 'CastError') return response.status(400).send({error: 'malformatted id'})
+  else if (error.name === 'ValidationError') return response.status(400).json({error: error.message})
+
+  next(error)
 }
 app.use(errorHandler)
 
