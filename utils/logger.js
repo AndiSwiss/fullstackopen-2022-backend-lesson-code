aa7c4a6f8e42e73cd5913f1862ea0a2cf112e7fb
Andreas Ambühl <36681000+andiswiss@users.noreply.github.com>
2022-08-13 11:45:31 +0200

    FEATURE: colored terminal output

--- utils/logger.js
+++ utils/logger.js
@@ -1,5 +1,9 @@
-const info = (...params) => console.log(...params)
+const colors = require('colors/safe')
 
-const error = (...params) => console.error(...params)
+// colored output (blue)
+const info = (...params) => console.log(colors.blue([...params].join(' ')))
+
+// colored output (red)
+const error = (...params) => console.error(colors.bgRed(['ERROR:', ...params].join(' ')))
 
 module.exports = { info, error }
