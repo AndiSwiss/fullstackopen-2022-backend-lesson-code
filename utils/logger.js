2c8eebbd736901792ddcc2b53dbceb2b8b36cefc
Andreas Ambühl <36681000+andiswiss@users.noreply.github.com>
2022-08-13 12:27:01 +0200

    uibuild

--- utils/logger.js
+++ utils/logger.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-console */
 const colors = require('colors/safe')
 
-// colored output (blue)
+// Colored output (blue)
 const info = (...params) => console.log(colors.blue(['INFO:', ...params].join(' ')))
 
-// colored output (red)
+// Colored output (red)
 const error = (...params) => console.error(colors.bgRed(['ERROR:', ...params].join(' ')))
 
 module.exports = { info, error }
