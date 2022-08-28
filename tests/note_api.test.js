f8c6097187ef571e3d126e53b0d40354168ce839
Andreas Ambühl <36681000+andiswiss@users.noreply.github.com>
2022-08-28 16:45:56 +0200

    FEATURE: tests are working now (with db-initialization)

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,8 +1,21 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
+const Note = require('../models/note')
+
+const initialNotes = [
+  { content: 'HTML is easy', date: new Date(), important: false },
+  { content: 'Browser can execute only Javascript', date: new Date(), important: true }
+]
+
+beforeEach(async () => {
+  await Note.deleteMany({})
+  for (const note of initialNotes) {
+    const noteObject = new Note(note)
+    await noteObject.save()
+  }
+})
 
 test('notes are returned as json', async () => {
   await api
@@ -12,11 +25,14 @@ test('notes are returned as json', async () => {
 }, 100000)
 
 test('there are two notes', async () => {
+  // IntelliJ thinks that the 'await' in the following line is not necessary, but it certainly is!!
+  // noinspection ES6RedundantAwait
   const response = await api.get('/api/notes')
   expect(response.body).toHaveLength(2)
 })
 
 test('the first note is about HTTP methods', async () => {
+  // noinspection ES6RedundantAwait
   const response = await api.get('/api/notes')
   expect(response.body[0].content).toBe('HTML is easy')
 })
