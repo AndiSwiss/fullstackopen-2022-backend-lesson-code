b800a7897a1ec98c1066f9b9f083e5fbf4b1efa9
Andreas Ambühl <36681000+andiswiss@users.noreply.github.com>
2022-09-04 14:39:16 +0200

    FEATURE: all tests pass - except when running with new IntelliJ run-config for all Jest tests

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -1,9 +1,21 @@
 const Note = require('../models/note')
 const User = require('../models/user')
+const bcrypt = require('bcrypt')
 
+/**
+ * Note: Technically, these notes are invalid, since no userId is defined
+ */
 const initialNotes = [
   { content: 'HTML is easy', date: new Date(), important: false },
   { content: 'Browser can execute only Javascript', date: new Date(), important: true }
+]
+
+/**
+ * Initial users - password hashing still needs to be done
+ */
+const initialUsers = [
+  { username: 'root', name: 'root user', password: 'sekret-93' },
+  { username: 'andi', name: 'Andi S', password: 'topSekret7' }
 ]
 
 const nonExistingId = async () => {
@@ -47,4 +59,33 @@ const usersInDb = async () => {
   return users.map(u => u.toJSON())
 }
 
-module.exports = { initialNotes, nonExistingId, notesInDb, usersInDb }
+const initializeNoteDb = async () => {
+  await Note.deleteMany({})
+  for (const note of initialNotes) {
+    const noteObject = new Note(note)
+    await noteObject.save()
+  }
+  //Other variant (also works) => https://fullstackopen.com/en/part4/testing_the_backend#optimizing-the-before-each-function
+  // const noteObjects = helper.initialNotes.map(note => new Note(note))
+  // const promiseArray = noteObjects.map(note => note.save())
+  // await Promise.all(promiseArray)
+}
+
+const initializeUserDb = async () => {
+  await User.deleteMany({})
+  for (const user of initialUsers) {
+    const passwordHash = await bcrypt.hash(user.password, 10)
+    const userObject = new User({ username: user.username, name: user.name, passwordHash })
+    await userObject.save()
+  }
+}
+
+module.exports = {
+  initialNotes,
+  initialUsers,
+  nonExistingId,
+  notesInDb,
+  usersInDb,
+  initializeNoteDb,
+  initializeUserDb
+}
--- tests/user_api.test.js
+++ tests/user_api.test.js
@@ -1,36 +1,27 @@
-const bcrypt = require('bcrypt')
 const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
-const User = require('../models/user')
 const helper = require('./test_helper')
 
-describe('when there is initially one user in db', () => {
-  beforeEach(async () => {
-    await User.deleteMany({})
-    const passwordHash = await bcrypt.hash('sekret', 10)
-    const user = new User({ username: 'root', passwordHash })
-    await user.save()
-  })
+beforeEach(async () => {
+  await helper.initializeUserDb()
+})
 
+describe('when there is initially one user in db', () => {
   test('creation succeeds with a fresh username', async () => {
     const usersAtStart = await helper.usersInDb()
-
     const newUser = {
       username: 'mluukkai',
       name: 'Matti Luukkainen',
       password: 'salainen'
     }
-
     await api
       .post('/api/users')
       .send(newUser)
       .expect(201)
       .expect('Content-Type', /application\/json/)
-
     const usersAtEnd = await helper.usersInDb()
     expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
-
     const usernames = usersAtEnd.map(u => u.username)
     expect(usernames).toContain('mluukkai')
   })
@@ -40,16 +31,14 @@ describe('when there is initially one user in db', () => {
     const newUser = {
       username: 'root',
       name: 'Superuser',
-      password: 'salainen',
+      password: 'salainen'
     }
     const result = await api
       .post('/api/users')
       .send(newUser)
       .expect(400)
       .expect('Content-Type', /application\/json/)
-
     expect(result.body.error).toContain('username must be unique')
-
     const usersAtEnd = await helper.usersInDb()
     expect(usersAtEnd).toEqual(usersAtStart)
   })
